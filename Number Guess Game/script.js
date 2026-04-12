0405d5a7099d5dc5ef78fdd217f6d012fed51ea3
MUHAMMAD TALHA <talha105th@gmail.com>
2026-04-12 11:30:29 +0500

    Fix comment formatting in script.js

--- Number Guess Game/script.js
+++ Number Guess Game/script.js
@@ -1,4 +1,4 @@
-    // Generate a random number between 1 and 100
+// Generate a random number between 1 and 100
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     
     // Function to check the guessed number
